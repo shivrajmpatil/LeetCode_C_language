ae27d0351d46d81d508e41911f6df2d38d03310b
shivraj patil <146567325+shivrajmpatil@users.noreply.github.com>
2024-02-09 10:06:32 +0530

    ZigzagPattern.c

--- ZigzagPattern.c
+++ ZigzagPattern.c
@@ -1,24 +1,31 @@
 #include<stdio.h>
 #include<conio.h>
 void main()
-{ char ch[]={“PAYPALISHIRING”},	ch1[20];
+
+{ char ch[]={"PAYPALISHIRING"},	ch1[20];
  int l,i,j,k,p=0;
  clrscr();
  l=strlen(ch);
- printf(“Given string=%s\n”,ch);
-for(i=0;i<3;i++)
-{ k=i;
-  for(j=0;j<l;j++)
-  { if(i%2==0&&(k==i||j%4==0))
-   printf(“%c”,ch1[p++]=ch[k]);
-   else if(i%2==1&&j%2==0)
-    printf(“%c”,ch1[p++]=ch[k]);
-   else
-    printf(“ “);
-   k++;
+ printf("Given string=%s\n",ch);
+
+ for(i=0;i<3;i++)
+  { k=i;
+    for(j=0;j<l;j++)
+     { 
+        if(i%2==0&&(k==i||j%4==0))
+        printf("%c",ch1[p++]=ch[k]);
+   
+        else if(i%2==1&&j%2==0)
+        printf("%c",ch1[p++]=ch[k]);
+   
+        else
+        printf(" ");
+  
+        k++;
+      }
+    printf("\n");
   }
-  printf(“\n”);
- }
- printf(“\nNew string=%s”,ch1);
+ 
+ printf("\nNew string=%s",ch1);
  getch();
-}
+ }
